ff5f71b97a6150b318f112337657d431f695f2d3
willyruh <wilfredkamau7854@gmail.com>
2026-01-31 16:44:03 +0300

    added some code to the files

--- app.py
+++ app.py
@@ -18,9 +18,15 @@ class IBAPP(IBWrapper, IBClient):
 
 if __name__ == "__main__":
     app = IBAPP("127.0.0.1", 7497, client_id=11)
+    eur = future("EUR", "CME", "202312")
+    for tick in app.get_streaming_data(99, eur):
+        print(tick)
     aapl = stock("AAPL", "SMART", "USD")
     gbl = future("GBL", "EUREX", "202403")
     pltr = options("PLTR", "BOX", "20240315", 20, "C")
+    data = app.get_historical_data(
+        request_id=99, contract=aapl, duration="2 D", bar_size="30 secs")
+    data = app.get_market_data(requst_di=99, contract=aapl)
     limit_order = limit(BUY, 100, 190.00)
     time.sleep(30)
     app.disconect()
--- wrapper.py
+++ wrapper.py
@@ -1,6 +1,35 @@
 from ibapi.wrapper import EWrapper
+import threading
 
 
 class IBWrapper(EWrapper):
     def __init__(self):
         EWrapper.__init__(self)
+        self.nextValidOrderId = None
+        self.streaming_data = {}
+        self.historical_data = {}
+        self.market_data = {}
+        self.streaming_event = threading.Event()
+
+    def tickByTickBidAsk(self, request_id, time, bid_price, bid_size, ask_size, tick_atrrib_last):
+        tick_data = (time, bid_price, bid_size, ask_size)
+        self.streaming_data[request_id] = tick_data
+        self.streaming_event.set()
+
+    def historicalData(self, request_id, bar):
+        bar_data = (
+            bar.data,
+            bar.open,
+            bar.high,
+            bar.low,
+            bar.close,
+            bar.volume,
+        )
+        if request_id not in self.historical_data.keys():
+            self.historical_data[request_id] = []
+        self.historical_data[request_id].append(bar_data)
+
+    def tickPrice(self, request_id, tick_type, price, attrib):
+        if request_id not in self.market_data.keys():
+            self.market_data[request_id] = {}
+        self.market_data[request_id][tick_type] = float(price)
